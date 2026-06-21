83b25a1af2c697077e713e18e7b7db5526e7c7dd
Mahadev Bolde <mahadevbolde36@gmail.com>
2026-06-22 02:43:13 +0530

    fix: server url in client

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import server from "../envirnment";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${server}/api`,
   withCredentials: true,
 });
 
