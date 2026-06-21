00a74460df5059c19b00217fd0d29a0798513081
Mahadev Bolde <mahadevbolde36@gmail.com>
2026-06-22 03:08:25 +0530

    fix: cors error

--- server/server.js
+++ server/server.js
@@ -20,7 +20,9 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin:
+      "https://sakshi-mehandi-portal-frontend.onrender.com/" ||
+      "http://localhost:5173",
     credentials: true,
   }),
 );
